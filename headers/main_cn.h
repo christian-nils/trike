9221f59798e583b09cac09ce97d67ab701272252
christian-nils <boda@chalmers.se>
2016-05-30 14:03:47 +0200

    fix test

--- headers/main_cn.h
+++ headers/main_cn.h
@@ -1,3 +1,4 @@
+#include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <iostream>
 #include <errno.h>
@@ -10,9 +11,26 @@ typedef unsigned char BOOL;
 typedef unsigned short UINT16;      // 16-bit variables
 typedef unsigned long UINT32;
 
+typedef struct HID_DESCRIPTOR_TABLE                                 // Struct to store HID descriptor fields
+{
+    UINT16 wHIDDescLen;                                             // MSB 1; LSB 0
+    UINT16 wRepDescLen;                                             // MSB 5; LSB 4
+    UINT16 wRepDescReg;                                             // MSB 6; LSB 7
+    UINT16 wMaxInpLen;                                              // MSB 11; LSB 10
+    UINT16 wCmdReg;                                                 // MSB 17; LSB 16
+    UINT16 wDatReg;                                                 // MSB 19; LSB 18
+    UINT16 wVenID;                                                  // MSB 21; LSB 20
+    UINT16 wProdID;                                                 // MSB 23; LSB 22
+    UINT16 wVerID;                                                  // MSB 25; LSB 24
+} HID_DESCRIPTOR_TABLE;
+
 // Define type
 #define TRUE 1
 #define FALSE 0
+
+#define CMD_LEN                 0x04
+#define POWER_CMD_MSB           0x08                                // POWER command bytes
+#define POWER_CMD_LSB           0x00
 
 #define SLAVE_ADDR 0x40
 extern int SLAVE_FD;
--- src/main_cn.cpp
+++ src/main_cn.cpp
@@ -43,10 +43,28 @@
 
 int main(void) 
 {
+	UINT8 ucTx_data[BUF_40];  
+	UINT8 ucCmdDatbuf[BUF_40];  
+	HID_DESCRIPTOR_TABLE HID_FIELD;                                     //structure for HID Descriptor 
 	int SLAVE_FD = wiringPiI2CSetup(SLAVE_ADDR);
 //	printf("%d", SLAVE_FD);
 	cout << "Init result: "<< SLAVE_FD << endl;
-	cout << "Trying to read from IMU" << endl;
-	cout << wiringPiI2CRead(SLAVE_FD) << endl;
-	
+	cout << "Trying to power the IMU" << endl;
+	ucTx_data[0] = HID_FIELD.wCmdReg;                       //command field bytes from HID config table 
+	ucTx_data[1] = (HID_FIELD.wCmdReg >> BYTE_SHIFT);                              
+	ucTx_data[2] = POWER_CMD_LSB;                           //HID Power command opcode low  byte for POWER Device ON
+	ucTx_data[3] = POWER_CMD_MSB;                           //HID Power command opcode high byte
+
+	ucRetStat = i2c_cmd_WrRd (WRITE,                        // Issue power on command to SSC7150
+						CMD_LEN,                            //num of cmd bytes
+						ucTx_data,                          //cmd buf
+						0,                                  //num of bytes to read
+						ucCmdDatbuf,                        //recv buf
+						FALSE);                             //flag indicating that we specified the number of bytes to read explicitly
+
+	if (ucRetStat != 0)
+		cout << "Fail" << endl;
+		return;                               // command failed
+//	cout << wiringPiI2CRead(SLAVE_FD) << endl;
+//	perror("wiringPiI2CRead()");
 }
