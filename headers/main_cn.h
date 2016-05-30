18a359a165e4b9b63cde28c7b2e40f97b708b461
christian-nils <boda@chalmers.se>
2016-05-30 10:06:07 +0200

    fix

--- headers/main_cn.h
+++ headers/main_cn.h
@@ -6,6 +6,17 @@
 // Custom headers
 #include "i2cIO.h"
 
+// Declare new types
+typedef unsigned char UINT8;        // 8-bit variables
+typedef unsigned char BOOL;
+   
+typedef unsigned short UINT16;      // 16-bit variables
+typedef unsigned long UINT32;
+
+// Define type
+#define TRUE 1
+#define FALSE 0
+
 #define SLAVE_ADDR 0x40
 extern int SLAVE_FD;
 
