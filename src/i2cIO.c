32e7eb19391b4765afa9733a89c65e033c766b1f
christian-nils <boda@chalmers.se>
2016-05-31 11:06:41 +0200

    f

--- src/i2cIO.c
+++ src/i2cIO.c
@@ -176,10 +176,10 @@ UINT8 i2c_cmd_WrRd(UINT8 ucCmd, UINT8 ucBytes_wr,  UINT8 *ucData_wr, UINT16 usBy
 
         case READ:
             printf("READ\n");
-            if (wiringPiI2CWrite(SLAVE_FD,SLAVE_ADDR))                        // Write slave address with the R/W indicator bit cleared
+            if (wiringPiI2CWrite(SLAVE_FD,(SLAVE_ADDR| 1)))                        // Write slave address with the R/W indicator bit cleared
                 i2cIO_error(WRITE_COLL);                            //write collision error display (does not return)
             
-//            MasterWaitForIntrI2C1();                                // Wait for Master interrupt request and then clear interrupt Flag.
+            MasterWaitForIntrI2C();                                // Wait for Master interrupt request and then clear interrupt Flag.
 
 //            if (!I2C1STATbits.ACKSTAT)                              // If ACK received then begin reading data from the slave
 //            {
@@ -196,10 +196,10 @@ UINT8 i2c_cmd_WrRd(UINT8 ucCmd, UINT8 ucBytes_wr,  UINT8 *ucData_wr, UINT16 usBy
 
         case WR_RD:
             printf("RW\n");
-            if (wiringPiI2CWrite(SLAVE_FD,SLAVE_ADDR))                        // Write slave address with the R/W indicator bit cleared
-                i2cIO_error(WRITE_COLL);                            //write collision error display (does not return)
-
-            MasterWaitForIntrI2C();                                // Wait for Master interrupt request and then clear interrupt Flag.
+//            if (wiringPiI2CWrite(SLAVE_FD,SLAVE_ADDR))                        // Write slave address with the R/W indicator bit cleared
+//                i2cIO_error(WRITE_COLL);                            //write collision error display (does not return)
+//
+//            MasterWaitForIntrI2C();                                // Wait for Master interrupt request and then clear interrupt Flag.
 //
 //            if (I2C1STATbits.ACKSTAT)                               // check for ACK condition
 //               i2cIO_error(NOT_ACK);                                // ACK error (does not return)
@@ -207,10 +207,10 @@ UINT8 i2c_cmd_WrRd(UINT8 ucCmd, UINT8 ucBytes_wr,  UINT8 *ucData_wr, UINT16 usBy
             for(i = 0; i < ucBytes_wr; i++)                         // Begin a loop writing the tx bytes to the slave
             {
 //                if (MasterWriteI2C1(ucData_wr[i]))                  // Write one byte of the tx data to the slave
-				if(wiringPiI2CWrite(SLAVE_FD, ucData_wr[i]))
+				if(wiringPiI2CWriteReg8(SLAVE_FD, 0, ucData_wr[i]))
                      i2cIO_error(WRITE_COLL);                       //write collision error (does not return)
                  
-printf("%d\n", ucData_wr[i]);   
+				printf("%d\n", ucData_wr[i]);   
 //                MasterWaitForIntrI2C1();                            // Wait for Master interrupt request and then clear interrupt Flag.
 
 //                if ((i + 1) < ucBytes_wr)                           // If this byte is not the last one, check ACK
