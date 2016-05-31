e263bb932f3c9968765ebc4e8c6ee090b5bdfac7
christian-nils <boda@chalmers.se>
2016-05-31 11:21:13 +0200

    f

--- src/vregs.c
+++ src/vregs.c
@@ -106,14 +106,14 @@ UINT8 VREG_init()
         if (ucRet) 
             return HID_INT_FAIL;
 	}
-
+hid_i2c_cmd_process(ucBuf, POWER_ON, ARB_ID);
     if ( hid_i2c_descriptor_handler(GET_HID_DESC) )                 // get HID descriptor from SSC7150
     {
         VREGS.stat.stat4.SHStartStatus = VREG_SHSTART_FAIL;         // update status register (VREG 0x3F) for failure to get HID descriptor
 //        return HID_DESC_FAIL;
     }        
     
-    hid_i2c_cmd_process(ucBuf, POWER_ON, ARB_ID);                   // Issue HID Power ON command to SSC7150 (NOTE: 'ucBuf' and 'ARB_ID' are don't cares for POWER_ON command)
+                       // Issue HID Power ON command to SSC7150 (NOTE: 'ucBuf' and 'ARB_ID' are don't cares for POWER_ON command)
     
     VREGS.SHC.reset = VREG_RESET_INIT;                              //set the SHC reset bit to indicate this operation has not yet completed successfully
     if ( hid_i2c_cmd_process (ucBuf, RESET_DEV_CMD, ARB_ID) )       // Issue HID Reset command  (NOTE: 'ucBuf' and 'ARB_ID' are don't cares for RESET_REG command)
