c54a47752a9f3a47d168b3fee075b7400d543689
christian-nils <boda@chalmers.se>
2016-06-03 10:15:46 +0200

    f

--- src/i2cIO.c
+++ src/i2cIO.c
@@ -90,15 +90,17 @@ UINT8 i2c_cmd_WrRd(UINT8 ucCmd, UINT8 ucBytes_wr,  UINT8 *ucData_wr, UINT16 usBy
             break;
 
         case WR_RD:
-			if(ret=i2c_smbus_read_i2c_block_data(SLAVE_FD, ucData_wr[1], usBytes_rd, ucData_rd)>0){
-//			if(i2c_smbus_write_i2c_block_data(SLAVE_FD, (UINT8)0x00, ucBytes_wr, ucData_wr))	
-				for (i=0;i++;i<ret)	{
-					printf("%d:", ucData_rd[i]);
-				}	
-				printf("\n");         
+			UINT8 cmd = ucData_wr[0];
+			// Shift from one byte
+			for (i=0;i++;i<ucBytes_wr-1){
+				ucData_wr[i] = ucData_wr[i+1];
+			}
+//			if(ret=i2c_smbus_read_i2c_block_data(SLAVE_FD, ucData_wr[1], usBytes_rd, ucData_rd)>0)
+			if(i2c_smbus_write_i2c_block_data(SLAVE_FD, cmd, ucBytes_wr-1, ucData_wr)<0){
+				printf("Error in i2c writing\n");         
 			}
 //			while(digitalRead(0) == 1);
-//            gets_I2C(ucData_rd, usBytes_rd, bAdjust);              // Read in multiple bytes
+//           gets_I2C(ucData_rd, usBytes_rd, bAdjust);              // Read in multiple bytes
             
             break;
     }
