d6f6a7eb0892382e3cc9b952a38848d7d98f0591
christian-nils <boda@chalmers.se>
2016-06-07 10:36:32 +0200

    f

--- src/i2cIO.c
+++ src/i2cIO.c
@@ -172,11 +172,10 @@ UINT8 i2c_cmd_WrRd(UINT8 ucCmd, UINT8 ucBytes_wr,  UINT8 *ucData_wr, UINT16 usBy
 //            gets_I2C(ucData_rd, usBytes_rd, bAdjust);              // Read in multiple bytes
 			if (ack = i2c_write_byte(TRUE,FALSE,SLAVE_ADDR))                               // check for ACK from slave
             {
-//				i2c_write_byte(TRUE,FALSE,SLAVE_ADDR);
-//                for(i = 0; i < ucBytes_wr; i++)                     // Begin a loop writing the tx bytes to the slave
-//                {              
-//					i2c_write_byte(FALSE, FALSE, ucData_wr[i]); 
-//                }
+                for(i = 0; i < ucBytes_wr; i++)                     // Begin a loop writing the tx bytes to the slave
+                {              
+					printf("%d\n",i2c_write_byte(FALSE, FALSE, ucData_wr[i])); 
+                }
             }            
             else
                 i2cIO_error(NOT_ACK);                              //ACK error (does not return)	   
